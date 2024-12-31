774fc0e80bf1feea651dcec7132d4c7f951fcf1a
ChitRanjan047 <aditya36187@gmail.com>
2024-12-31 18:58:55 +0530

    Update Sphere Calculator.py

--- Sphere Calculator.py
+++ Sphere Calculator.py
@@ -2,19 +2,27 @@ import math
 # Sphere Area And Volume Calculator
 
 print("Welcome To Sphere Area And Volume Calculator.")
-R = int(input("Enter The Value Of Radius:"))
-X = 4*math.pi*R*R
-Y = 4/3*math.pi*R*R*R
-print('''Choose An Action :
-        1.Surface Area - A
-        2.Volume - V ''')
-Action = input("What Do You Want To Do?")
-if Action=='A' :
-    print("Answer:", X)
-elif Action=='V' :
-    print("Answer:", Y)
-else :
-    print("Command Undefined.")
-    
 
+while True:
+    R = int(input("Enter The Value Of Radius:"))
+    X = 4*math.pi*R*R
+    Y = (4/3)*math.pi*R*R*R
+    print('''Choose An Action :
+            1.Surface Area - A
+            2.Volume - V ''')
+    Action = input("What Do You Want To Do?")
+    if Action=='A' :
+        print("Answer:", X)
+    elif Action=='V' :
+        print("Answer:", Y)
+    else :
+        print("Command Undefined.")
+
+    Z = input("Do you want to calculate again?[Y/N]")
+
+    if Z=='N':
+        print("Thank You!!!")
+        break
+    else:
+        continue
 
